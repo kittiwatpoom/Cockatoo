559a023554a2b3c44605c289c879a7c826b184bf
poomktw <kittiwat.ktw@mail.kmutt.ac.th>
2019-08-26 15:46:52 +0700

    HelloWorld2019

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="container">
+      <button type="button" class="btn btn-danger">Danger</button>
+    </div>
+  );
+}
+
+export default App;
